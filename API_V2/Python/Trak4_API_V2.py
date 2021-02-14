e9a4c602b64c8033255f7251c2aab949436b7ada
Brian-Webster <brianwebster@gmail.com>
2021-02-14 17:56:44 -0600

    Modified request JSON variable name for clarity

--- API_V2/Python/Trak4_API_V2.py
+++ API_V2/Python/Trak4_API_V2.py
@@ -4,12 +4,11 @@ import json
 API_URL = "https://gps.trak-4.com/api/v2/";    
 API_KEY = "Your_API_KEY Here";  
 
-myobj = {'commandstring': 'get_devices','token':API_KEY}
+Request_Payload = {'commandstring': 'get_devices','token':API_KEY}
 
+x = requests.post(API_URL, json=Request_Payload)
 
-
-
-x = requests.post(API_URL, json=myobj)
 json_data = x.json()
+
 for device in json_data.get("data"):
     print ("Device# " + str(device.get("deviceId")) + " last reported at " + str(device.get("lastReportTime")))
