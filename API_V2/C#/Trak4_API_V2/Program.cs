e7f04a3eee430b63ae73f8473b00735dd52b2a5c
Brian-Webster <brianwebster@gmail.com>
2021-02-14 18:01:05 -0600

    improved readability

--- API_V2/C#/Trak4_API_V2/Program.cs
+++ API_V2/C#/Trak4_API_V2/Program.cs
@@ -14,31 +14,23 @@ namespace Trak4API
             const string API_URL = "https://gps.trak-4.com/api/v2/";    //api URL 
             const string API_KEY = "YOUR_API_KEY";  //Your API Key
 
-
             var jsonInString = "{\"commandstring\":\"get_devices" +     //JSON in string form
                 "\",\"token\":\"" + API_KEY + "\",}";
-
 
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri(API_URL);
 
-            client.DefaultRequestHeaders.Accept.Add(
-            new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            
             var response = client.PostAsync(API_URL,
-                                      new StringContent(jsonInString, Encoding.UTF8, "application/json")).Result;
+                                              new StringContent(jsonInString, Encoding.UTF8, "application/json")).Result;
+            
             var res = response.Content.ReadAsStringAsync().Result;
+            
             dynamic json = JsonConvert.DeserializeObject(res);
 
-
             foreach (var x in json.data)
-            {
                 Console.WriteLine("Device# " + x.deviceId.ToString() + " last reported " + x.lastReportTime);
-
-            }
-
-
-
-
 
         }
     }
