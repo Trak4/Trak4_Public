39166b6e09c169d68402babf5b784b1cb1972744
Brian-Webster <brianwebster@gmail.com>
2021-02-19 11:27:56 -0600

    fetch api request sample

--- API_V2/Typescript_React/my-app/src/App.tsx
+++ API_V2/Typescript_React/my-app/src/App.tsx
@@ -10,20 +10,37 @@ function App() {
   let a: string[] = [];
   const [data, setData] = useState<string[]>([]);
   const [isLoading, setLoading] = useState<boolean>(false);
+
+
   const fetchData = () => {
     setLoading(true);
-    axios
-      .post<any>("https://gps.trak-4.com/api/v2/", {
-        commandstring: "get_devices",
-        token: "d1b95a4c22f546faa851a8961e0d20f9",
-      })
-      .then((resp) => {
-        resp.data.data.forEach((element: any) => {
-          a.push(element.deviceId);
-        });
-        setData(a);
-        setLoading(false);
-      });
+
+    // axios
+    //   .post<any>("https://gps.trak-4.com/api/v2/", {
+    //     commandstring: "get_devices",
+    //     token: "d1b95a4c22f546faa851a8961e0d20f9",
+    //   })
+
+    //   .then((resp) => {
+    //     resp.data.data.forEach((element: any) => {
+    //       a.push(element.deviceId);
+    //     });
+    //     setData(a);
+    //     setLoading(false);
+    //   });
+
+
+    getAllTrak4Devices()
+         .then((retVal: any) => {
+            let assetArray = retVal.data;
+            assetArray.forEach((element: any) => {
+               console.log(element);
+            });
+         })
+         .catch((err: any) => {
+            console.log(err);
+         });
+
   };
 
   return (
@@ -50,5 +67,45 @@ function App() {
     </div>
   );
 }
+
+
+
+
+
+const getAllTrak4Devices = (): any => {
+  let myHeaders = new Headers();
+
+  let data = {
+     commandstring: "get_devices",
+     token: 'd1b95a4c22f546faa851a8961e0d20f9',
+  };
+
+  myHeaders.append("Content-Type", "text/plain");
+  myHeaders.append("Accept", "*/*");
+  myHeaders.append("Connection", "keep-alive");
+
+  const myInit = {
+     method: "POST",
+     headers: myHeaders,
+     body: JSON.stringify(data),
+  };
+  
+  return new Promise((resolve) => {
+     fetch('https://gps.trak-4.com/api/v2/', myInit)
+        .then((res) => {
+           switch (res.status) {
+              default:
+                 res.json().then((data: any) => {
+                    resolve(data);
+                 });
+           }
+        })
+        .catch((err) => alert(err));
+  });
+
+};
+
+
+
 
 export default App;
